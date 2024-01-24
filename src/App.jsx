cff16c0f7f52234928a82e859223ac1c93b1ed0c
John Doe <johndoe@example.com>
2024-01-24 18:20:21 +0100

    realizando menu lateral

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import Header from "./components/Header";
+import AsideMenu from "./components/AsideMenu";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Header />
+      <AsideMenu />
       <main>
         <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-          <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
-            Hello, Brandon.
-          </h1>
+          <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name"></h1>
         </section>
       </main>
     </>
